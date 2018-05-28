3a07c02c3ea24d7689bcc9efc146880c85e3914f
Sockshead <juancapoar@gmail.com>
2018-05-27 19:58:20 -0500

    fix

---
+++ build/web/home.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : home
+    Created on : May 27, 2018, 7:43:19 PM
+    Author     : juanc
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <p> ${empleado.nombres} </p>
+        <p> ${empleado.usuario} </p>
+        <p> ${empleado.eps} </p>
+        <p> ${empleado.arl} </p>
+        <p> ${empleado.rh} </p>
+        <p> ${empleado.cj} </p>
+        <p> ${empleado.fp} </p>
+        <p> ${empleado.idepartamento} </p>
+    </body>
+</html>
--- src/java/SQL/Conexion.java
+++ src/java/SQL/Conexion.java
@@ -1,29 +1,78 @@
-
 package SQL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author juanc
+ * @author Nikolas
  */
 public class Conexion {
-    Connection conectar = null;
+     public Connection conectar (){
+    Connection con =null;
+    String url = "jdbc:mysql://localhost:3306/thejourneygroup";
+    try {
+    Class.forName("com.mysql.jdbc.Driver");
+    con = DriverManager.getConnection(url,"root","Yosoyjcpa16.");
+    System.out.println("Conexión establecida como root");
+
+ }
+    catch (ClassNotFoundException | SQLException e){
+        System.err.println("Error");
+    }
+    return con;
+    }
+  
     
-    public Conexion(){
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/thejourneygroup?autoReconnect=true&useSSL=false","root","Yosoyjcpa16.");
-            System.out.print("aklsjdakdla");
-        }catch(Exception e){
-            System.err.println(e.getMessage());
+     
+     
+     
+  public ResultSet ObtenerTodo (String NombreCol1){
+Connection con = conectar();
+ResultSet rs = null;
+PreparedStatement ps = null;
+try{
+ ps = con.prepareStatement("select " + NombreCol1+ " from empleado"+";");
+
+rs = ps.executeQuery();
+
+}
+catch(Exception e){
+    System.err.println("Error en método Obtener"+e);
+
+} 
+return rs;
+
+}
+  
+  public ResultSet ObtenerTodoUltimo (String NombreCol1, String NombreTabla){
+Connection con = conectar();
+ResultSet rs = null;
+PreparedStatement ps = null;
+try{
+ ps = con.prepareStatement("select " + NombreCol1+ " from "+" "+NombreTabla+" ORDER BY "+NombreCol1+ " DESC LIMIT 1;");
+
+rs = ps.executeQuery();
+
+}
+catch(Exception e){
+    System.err.println("Error en método Obtener"+e);
+
+} 
+return rs;
+
+}
+   public void disconnect(Connection con){
+        System.out.println("Closing database: ["+ con +"] OK");
+        if (con != null) {
+            try{
+                con.close();
+            }catch (SQLException e){
+                System.out.println(e);
+            }
         }
-    }
-    
-    public Connection getConexion(){
-        return conectar;
-    }
-    
-    
-    
+   }
 }
